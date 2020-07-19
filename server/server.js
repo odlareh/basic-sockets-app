722edd15a545160979b0d33e9efb51386a492c4c
HSM <sanmartin.heraldo@gmail.com>
2020-07-19 01:30:42 -0400

    Backend socket configuration.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
+const http = require('http');
+const server = http.createServer(app);
 require('./config');
 
 // Parse application/x-www-form-urlencoded
@@ -9,12 +13,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Parse application/json
 app.use(bodyParser.json());
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hola a tod@s!')
-})
+// Enable public folder
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+// Enable comunication with backend
+let io = socketIO(server);
 
 // Port to listen
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log('Escuchando en puerto: ' + process.env.PORT)
 });
